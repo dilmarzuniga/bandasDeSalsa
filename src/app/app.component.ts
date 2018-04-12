0b4bf82613f39470b2b5e986c055e2b5d36eb7ae
dilmarzuniga <dilmaridelver@gmail.com.co>
2018-04-12 16:29:13 -0500

    guia 2.4 terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,24 @@ export class Banda {
   nombre: string;
 }
 
+const BANDAS: Banda[] = [
+  { id:11, nombre:'Hermanos Lebrón' },
+  { id:12, nombre:'Fania all-stars' },
+  { id:13, nombre:'Grupo Niche'},
+  { id:14, nombre:'Orquesta Guayacán' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Mis bandas de salsa';
-  banda: Banda = {
-    id: 1,
-    nombre: 'los hermanos lebrón'
+  titulo= 'Mis bandas de salsa';
+  bandas= BANDAS;  
+
+  banda: Banda;
+  seleccionar(item: Banda): void {
+    this.banda= item;
   }
-  
 }
